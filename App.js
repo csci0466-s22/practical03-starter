38e7f85692d4292ed5576f389d51d7612ea8708f
Christopher Andrews <candrews@middlebury.edu>
2022-04-08 01:29:27 -0400

    Starter code for practical 3

---
+++ App.js
@@ -0,0 +1,15 @@
+import { View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+
+
+export default function App() {
+  return (
+    <View >
+
+      <StatusBar style="auto" />
+
+    </View>
+  );
+}
+
